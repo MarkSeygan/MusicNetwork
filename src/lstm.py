64b5c39e776346c8daff868b31af22e433db0cc7
Mark Seygan <marekzidek@centrum.cz>
2017-02-22 00:00:25 +0100

    made the last activation softmax and categorial cross-entropy loss\nstarted working on second NN for nr of notes to be sampled from the distribution of first NN\n git add . gen Function not yet compatible with the new distribution approach (sampling from distribution needed)

--- src/lstm.py
+++ src/lstm.py
@@ -18,12 +18,20 @@ class Model(object):
         self.inputSize = 160  # as outpuSize, but with 4 bits for beat
         self.outputSize = 156 # noteMatrix span times 2 for ligature
 
-        self.theModel = StackedCells(self.inputSize, celltype=LSTM, layers=layerSizes)
-        self.theModel.layers.append(Layer(self.layerSizes[-1], self.outputSize, activation=T.nnet.sigmoid))
+        self.notes_distribution_model = StackedCells(self.inputSize, celltype=LSTM, layers=layerSizes)
+        #change self.notes_distribution_model.layers.append(Layer(self.layerSizes[-1], self.outputSize, activation=T.nnet.sigmoid))
+        self.notes_distribution_model.layers.append(Layer(self.layerSizes[-1], self.outputSize, activation=T.nnet.softmax))
+
+        notes_to_play_hidden_sizes = [50, 50]
+        self.nr_notes_to_play_model = StackedCells(self.inputSize, celltype=LSTM, layers=[])
+        self.nr_notes_to_play_model.layers.append(Layer(notes_to_play_hidden_sizes[-1], 1, activation=None)) # try with ReLU here
+
+
 
         self.rng = T.shared_randomstreams.RandomStreams(np.random.randint(0,1000))
 
         # here you can modify percentage of neurons being zeroed agains overfitting
+        # two methods at inference
         self.dropout = 0.5
 
         print "setting up train"
@@ -34,20 +42,20 @@ class Model(object):
 
     @property
     def params(self):
-        return self.theModel.params
+        return self.notes_distribution_model.params
 
     @params.setter
     def params(self, params):
-        self.theModel.params = params
+        self.notes_distribution_model.params = params
 
     @property
     def config(self):
-        return [self.theModel.params, [l.initial_hidden_state for l in self.theModel.layers if hasattr(l, 'initialHiddenState')]]
+        return [self.notes_distribution_model.params, [l.initial_hidden_state for l in self.notes_distribution_model.layers if hasattr(l, 'initialHiddenState')]]
 
     @config.setter
     def config(self, config):
-        self.theModel.params = config[0]
-        for l, val in zip((l for l in self.theModel.layers if hasattr(l, 'initialHiddenState')), config[1]):
+        self.notes_distribution_model.params = config[0]
+        for l, val in zip((l for l in self.notes_distribution_model.layers if hasattr(l, 'initialHiddenState')), config[1]):
             l.initial_hidden_state.set_value(val.get_value())
 
     def setTrainingFunction(self):
@@ -57,13 +65,15 @@ class Model(object):
         # matrix play and ligature
         # int8 also for binary logical operations
 
+        #######################################
+        ## notes_distribution_model training ##
+        #######################################
         self.inputMatrix = T.bmatrix()  # time, #beat#note#ligature#....
         self.outputMatrix = T.btensor3()  # time, note, ligature
 
         #@@@ for batch training use: 
         #@@@ self.inputMatrix = T.btensor3()
         #@@@ self.outputMatrix = T.btensor4()
-
 
         def step(inputData, *other):
             other = list(other)
@@ -73,7 +83,7 @@ class Model(object):
             else:
                 hiddens = other
                 dropout = []
-            newStates = self.theModel.forward(inputData, prev_hiddens=hiddens, dropout=dropout)
+            newStates = self.notes_distribution_model.forward(inputData, prev_hiddens=hiddens, dropout=dropout)
             return newStates
 
         inputWithoutLastTime = self.inputMatrix[0:-1] # because last time will be predicted
@@ -91,8 +101,8 @@ class Model(object):
             masks = []
 
         # give all layers some initial value from time before the song starts
-        outputsInfo = [initialState(layer) for layer in self.theModel.layers]
-        #@@@ outputsInfo = [initialState(layer, batchSize) for layer in self.theModel.layers]
+        outputsInfo = [initialState(layer) for layer in self.notes_distribution_model.layers]
+        #@@@ outputsInfo = [initialState(layer, batchSize) for layer in self.notes_distribution_model.layers]
 
         result, _ = theano.scan(fn=step, sequences=inputWithoutLastTime, non_sequences=masks, outputs_info=outputsInfo)
 
@@ -109,6 +119,8 @@ class Model(object):
         # keep it in 3 dims means -> pads the resulting played notes into arrays with one element
         activeNotes = T.shape_padright(self.outputMatrix[1:, :, 0])
         #@@@ activeNotes = T.shape_padright(self.outputMatrix[:, 1:, :, 0])
+
+        # !!! ted kdyz tomu vic rozumim, pocekovat, jestli je ta maska spravne !!!
         mask = T.concatenate([T.ones_like(activeNotes), activeNotes], axis=2)
         #@@@ mask = T.concatenate([T.ones_like(activeNotes), activeNotes], axis=3)
 
@@ -116,20 +128,34 @@ class Model(object):
         # is added to the probabilities to avoid logarithm from zero and NaNs
         self.eps = np.spacing(np.float32(0.0))
 
-        # Cross-entropy cost:
-        # mathematical if -> we get probabulity P from network and compare to the correct output X:
+        # Negative log-likelihood:
+        # Cross-entropy between true and predicted distribution:
+        # In binary decisions can be understood as mathematical if -> we get probabulity P from network and compare to the correct output X:
         # V = (1-P)(1-X) + PX .... one side would be multiplied by 0 and the second 1 everytime so it kinda pattern matches the right probability, where we
         # want V to be as big as possible. If V is low, it is bad -> increase cost
         # let's use negative logarithm, which nicely enlarges the cost if the P from network doesn't match the real output,
         # and makes the cost really tiny as P is getting closer to real output
-        probs = mask * T.log((1-final)*(1-self.outputMatrix[1:]) + final*self.outputMatrix[1:] + self.eps)
+         
+        #change probs = mask * T.log((1-final)*(1-self.outputMatrix[1:]) + final*self.outputMatrix[1:] + self.eps)
+        self.cost = mask * T.nnet.categorial_cross_entropy(final, self.outputMatrix[1:])
+
         #@@@ probs = mask * T.log((1-final)*(1-self.outputMatrix[:,1:]) + final*self.outputMatrix[:,1:] + self.eps)
 
         # and sum from the cross-entropy formula
-        self.cost = T.neg(T.sum(probs))
+        # this line not needed since cross entropy already calculates the cost       self.cost = T.neg(T.sum(probs))
 
         # adadelta should adaptively adjust the learning rates so we should't bother
         updates, _, _, _, _ = create_optimization_updates(self.cost, self.params, method="adadelta")
+
+        #####################################
+        ## nr_notes_to_play_model training ##
+        #####################################
+
+        # self.inputMatrix is the same as for the other model in this scope
+        self.output_nr_notes = T.bmatrix() # time, amount
+        input = self.inputMatrix[0:-1] # last time will be predicted
+        
+
 
         self.trainingFunction = theano.function(
             inputs=[self.inputMatrix, self.outputMatrix],
@@ -156,23 +182,25 @@ class Model(object):
             currTime = states[-1]
 
             if self.dropout > 0:
-                masks = [1 - self.dropout for layer in self.theModel.layers]
+                masks = [1 - self.dropout for layer in self.notes_distribution_model.layers]
                 masks[0] = None
             else:
                 masks = []
 
-            newStates = self.theModel.forward(inputData, prev_hiddens=hiddens, dropout=masks)
+            newStates = self.notes_distribution_model.forward(inputData, prev_hiddens=hiddens, dropout=masks)
 
             probs = getLastLayer(newStates)
 
             finalBig = []
             for i in range(0, self.outputSize):
                 if i % 2 == 0:
-                    # that magic number is adjusting probs at 1 < means more errors, less silent moments
-                    finalBig.append(self.rng.uniform() < probs[i] ** 0.9)
+                	# !!! guilty code here !!!
+                	magicNumber = 1.1 # that magic number is adjusting probs where < 1 means more errors, less silent moments
+                    finalBig.append(self.rng.uniform() < probs[i] ** magicNumber)
                 else:
-                    # and here > 1 means more of ligatures
-                    finalBig.append(finalBig[-1] * (self.rng.uniform() < probs[i] ** 1.1))
+                	magicNumber = 1.1 # and here > 1 means more of ligatures
+                    prev_note_played = finalBig[-1]
+                    finalBig.append(prev_note_played * (self.rng.uniform() < probs[i] ** magicNumber))
 
             chosen = T.stack(finalBig)
 
@@ -182,7 +210,7 @@ class Model(object):
 
             return listify(newStates) + [nextInput, currTime+1, final]
 
-        outputsInfo = ([initialState(layer) for layer in self.theModel.layers] +
+        outputsInfo = ([initialState(layer) for layer in self.notes_distribution_model.layers] +
                         [dict(initial=self.startSeed, taps=[-1]), dict(initial=self.currTime, taps=[-1]), None])
 
         result, updates = theano.scan(fn=step, outputs_info=outputsInfo, n_steps=self.num_steps)
